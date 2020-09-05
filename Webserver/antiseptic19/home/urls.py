4a24b5210cd2a15302a425aed4edc4c44afc442a
hyewon <khhw2126@gmail.com>
2020-09-06 00:45:50 +0900

    home APP : make_room 틀

--- Webserver/antiseptic19/home/urls.py
+++ Webserver/antiseptic19/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('test',views.test, name="test"),
     path('make_room', views.make_room, name="make_room"),  # 방만들기
     path('enter_room', views.enter_room, name="enter_room"),  # 방들어가기
 
