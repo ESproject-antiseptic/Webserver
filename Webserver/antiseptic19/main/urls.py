a6dc4f85f5c9d7e11a826fe67e59c76b5d0b59d4
hyewon <khhw2126@gmail.com>
2020-09-14 20:45:02 +0900

    #9.14 모두 merge

--- Webserver/antiseptic19/main/urls.py
+++ Webserver/antiseptic19/main/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='main'),
-    path('home/',views.home, name = "home"),
     path('signup', views.signup, name='signup'),
     path('logout',views.logout,name='logout'),
     path('home/mypage',views.mypage, name="mypage"),
